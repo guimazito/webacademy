d64a74ded0fd8b346913abb327b840cdc2727462
Claudio Albano Guimaraes <albano.guimaraes@gmail.com>
2025-05-11 19:22:35 -0400

    add show file content feature

--- WACAD007-NODE/node1/index.js
+++ WACAD007-NODE/node1/index.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
+const url = require('url');
 const http = require('http');
 const dotenv = require('dotenv');
+const { createLink, goHome } = require('./utils/util.js');
 
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
@@ -13,17 +15,33 @@ if (!path) {
 }
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/html;charset=utf-8" })
-    fs.readdir(path, (err, files) => {
-        if (err) res.writeHead(err);
-        else {
-            res.write("<h1>Arquivos no diretório:</h1>");
-            files.forEach(file => {
-                res.write(`<p>${file}</p>`);
-            });
-        }
-        res.end();
-    })
+    if (req.url === "/") {
+        res.writeHead(200, { "Content-Type": "text/html;charset=utf-8" })
+        fs.readdir(path, (err, files) => {
+            if (err) res.writeHead(err);
+            else {
+                res.write("<h1>Arquivos no diretório:</h1>");
+                files.forEach(file => {
+                    res.write(`${createLink(file)}`);
+                });
+                res.end();
+            }
+        })
+    } else {
+        const parsedUrl = url.parse(req.url);
+        const fileName = parsedUrl.path.replace("/", "");
+        const filePath = `${path}/${fileName}`;
+        fs.readFile(filePath, "utf-8", (err, content) => {
+            if (err) {
+                res.writeHead(500, { "Content-Type": "text/html;charset=utf-8" });
+                res.end("Erro ao ler o arquivo.");
+            } else {
+                res.writeHead(200, { "Content-Type": "text/html;charset=utf-8" })
+                res.write(`${goHome()}`);
+                res.end(content);
+            }
+        });
+    }
 });
 
 server.listen(PORT, () => {
