dee47f12cbeb40d2e42f8b880d131a004a500841
Claudio Albano Guimaraes <albano.guimaraes@gmail.com>
2025-06-19 19:18:54 -0400

    change validate logic

--- WACAD011-Cookies/ExpAPI/src/middlewares/validate.ts
+++ WACAD011-Cookies/ExpAPI/src/middlewares/validate.ts
@@ -1,13 +1,13 @@
 import { Schema } from "joi";
 import { StatusCodes } from "http-status-codes";
-import { NextFunction, Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 
 export const validate = (schema: Schema) => {
     return (req: Request, res: Response, next: NextFunction) => {
-        const result = schema.validate(req.body);
-        if (result.error) {
-            return res.status(StatusCodes.BAD_REQUEST).send(result.error.details[0].message);
-        }
-        next();
+        const { error } = schema.validate(req.body, {
+            abortEarly: false
+        });
+        if (error) res.status(StatusCodes.BAD_REQUEST).json({ error: error.details });
+        else next();
     };
 };
