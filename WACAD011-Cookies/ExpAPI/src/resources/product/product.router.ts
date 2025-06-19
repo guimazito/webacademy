5db9a96bb2d8016cda660e7aa975a768560c88a5
Claudio Albano Guimaraes <albano.guimaraes@gmail.com>
2025-06-19 19:22:48 -0400

    add validate parameter in the routers

--- WACAD011-Cookies/ExpAPI/src/resources/product/product.router.ts
+++ WACAD011-Cookies/ExpAPI/src/resources/product/product.router.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import productController from "./product.controller";
 import { validate } from "../../middlewares/validate";
-import { productSchema } from "./product.schema";
+import { productSchema, productIdSchema } from "./product.schema";
 
 const router = Router();
 
 router.get("/", productController.index);
 router.post("/", validate(productSchema), productController.create);
-router.get("/:id", productController.read);
-router.put("/:id", productController.update);
-router.delete("/:id", productController.remove);
+router.get("/:id", validate(productIdSchema), productController.read);
+router.put("/:id", validate(productSchema), productController.update);
+router.delete("/:id", validate(productIdSchema), productController.remove);
 
 export default router;
