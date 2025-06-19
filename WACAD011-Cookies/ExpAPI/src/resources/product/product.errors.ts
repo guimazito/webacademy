1a8830e09cbbba109b7a6f1f55c021c714149e4b
Claudio Albano Guimaraes <albano.guimaraes@gmail.com>
2025-06-19 19:21:35 -0400

    add http-status-codes library

--- WACAD011-Cookies/ExpAPI/src/resources/product/product.errors.ts
+++ WACAD011-Cookies/ExpAPI/src/resources/product/product.errors.ts
@@ -1,19 +1,25 @@
 import { Response } from "express";
 import { Prisma } from "@prisma/client";
+import { StatusCodes/*, ReasonPhrases*/ } from "http-status-codes";
 
-export const createProductError = (req: Request, res: Response, err: unknown) => {
+export const productError = (res: Response, err: any) => {
     if (err instanceof Prisma.PrismaClientValidationError) {
-        res.status(400).json({
+        res.status(StatusCodes.BAD_REQUEST).json({
             error: "Validation Error",
-            message: "The data provided is invalid. ",
+            message: "The data provided is invalid.",
         });
     } else if (err instanceof Prisma.PrismaClientKnownRequestError) {
-        res.status(400).json({
+        res.status(StatusCodes.BAD_REQUEST).json({
+            error: "Database Error",
+            message: err.message,
+        });
+    } else if (err instanceof Prisma.PrismaClientInitializationError) {
+        res.status(StatusCodes.BAD_REQUEST).json({
             error: "Database Error",
             message: err.message,
         });
     } else {
-        res.status(500).json({
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
             error: "Internal Server Error",
             message: "Something went wrong. Please try again later.",
         });
