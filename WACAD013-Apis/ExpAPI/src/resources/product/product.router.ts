59eaf0e949faae8d1c8bc230eb868e926ab009a4
Claudio Albano Guimaraes <albano.guimaraes@gmail.com>
2025-07-06 08:18:54 -0400

    add isAuth is all product routers

--- WACAD013-Apis/ExpAPI/src/resources/product/product.router.ts
+++ WACAD013-Apis/ExpAPI/src/resources/product/product.router.ts
@@ -3,13 +3,14 @@ import productController from "./product.controller";
 import { validate } from "../../middlewares/validate";
 import { productSchema, productIdSchema } from "./product.schema";
 import { isAdmin } from "../../middlewares/isAdmin";
+import { isAuth } from "../../middlewares/isAuth";
 
 const router = Router();
 
-router.get("/", productController.index);
+router.get("/", isAuth, productController.index);
 router.post("/", isAdmin, validate(productSchema), productController.create);
-router.get("/:id", validate(productIdSchema), productController.read);
-router.put("/:id", validate(productSchema), productController.update);
-router.delete("/:id", validate(productIdSchema), productController.remove);
+router.get("/:id", isAuth, validate(productIdSchema), productController.read);
+router.put("/:id", isAuth, validate(productSchema), productController.update);
+router.delete("/:id", isAuth, validate(productIdSchema), productController.remove);
 
 export default router;
