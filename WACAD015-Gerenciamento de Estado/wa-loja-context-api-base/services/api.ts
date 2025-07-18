9b7aa4b8789b844bd523e32504f438c9c8966dfc
Claudio Albano Guimaraes <albano.guimaraes@gmail.com>
2025-07-18 19:32:20 -0400

    remove useState from ListagemFavoritos

--- WACAD015-Gerenciamento de Estado/wa-loja-context-api-base/services/api.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const apiProdutos = axios.create({
-    baseURL: "https://ranekapi.origamid.dev/json/api",
-});
-
-export const apiFavoritos = axios.create({
-    baseURL: "https://favoritos-json-server-claudio-albano-guimaraes-projects.vercel.app",
-});
