f4d6dd7146af1021eb86fa1658a450551e12f507
Claudio Albano Guimaraes <albano.guimaraes@gmail.com>
2025-07-26 09:40:19 -0400

    fix favoritos null or undefined

--- WACAD016-Fundamentos de Teste de Software/wa-loja-testes/src/app/components/ListagemFavoritos/ListagemFavoritos.tsx
+++ WACAD016-Fundamentos de Teste de Software/wa-loja-testes/src/app/components/ListagemFavoritos/ListagemFavoritos.tsx
@@ -15,7 +15,7 @@ export default function ListagemFavoritos() {
       <div className="row card-body">
         <h5 className="card-title mb-4 fw-bold">Lista de favoritos:</h5>
 
-        {favoritos.length > 0 ? (
+        {(favoritos && favoritos.length > 0) ? (
           <div className="table-responsive">
             <table className="table table-borderless">
               <thead>
@@ -43,7 +43,7 @@ export default function ListagemFavoritos() {
       </div>
       <div className="card-footer d-flex flex-column">
         <small className="text-muted">
-          Quantidade de produtos: {favoritos.length}
+          Quantidade de produtos: {favoritos ? favoritos.length : 0}
         </small>
 
         <small className="text-muted">
